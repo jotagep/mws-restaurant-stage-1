24f8458b7000b3282243f79b8b35eb33707691f7
Jorge <jotagmzn@gmail.com>
2018-04-11 13:55:17 +0200

    Add gulp task 'serve'

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+    browserSync.init({
+        port: 8000,
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch(['./*.html', './assets/css/*.css', './js/*.js']).on('change', reload);
+});
+
 
 
