a607424a246758b6b281bf3dcc403e76008cf3a3
Jorge <jotagmzn@gmail.com>
2018-04-13 12:40:54 +0200

    Add fonts, and add css properties (mobile first) and some classes to do the grid

--- js/main.js
+++ js/main.js
@@ -140,23 +140,27 @@ createRestaurantHTML = (restaurant) => {
   li.className = 'card'
 
   const image = document.createElement('img');
-  image.className = 'restaurant-img';
+  image.className = 'card__img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
+  name.className = 'card__title';
   name.innerHTML = restaurant.name;
   li.append(name);
 
   const neighborhood = document.createElement('p');
+  neighborhood.className = 'card__description';
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
 
   const address = document.createElement('p');
+  address.className = 'card__description';
   address.innerHTML = restaurant.address;
   li.append(address);
 
   const more = document.createElement('a');
+  more.className = 'card__button';
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
